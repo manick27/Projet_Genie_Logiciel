2936fd24c748a08418e90877e7bc6eb8ab145cc3
nguewouo mabkop manick <nguewouom@gmail.com>
2021-12-30 21:34:54 +0100

    Réinitialisation du projet avec react

--- pharmacie/sudheader.php
+++
@@ -1,8 +0,0 @@
-<link href="style.css" rel="stylesheet">
-<script type="text/javascript" src="js/slider.js"></script>
-<script type="text/javascript">setInterval("changeSlide(1)", 5000);</script>
-<div id="subheader">
-	<div id="slider">
-		<img src="images/img0.jpg" alt="slide" id="slide">
-	</div>
-</div>
